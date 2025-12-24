0768d1f6057a5d26500c2e82bebd6fea6a784ca5
PTPhongKMF <169053895+PTPhongKMF@users.noreply.github.com>
2025-12-24 16:59:29 +0700

    refactor: change prepareEnvironment log to debug

--- src/workflows/prepare-environment.ts
+++ src/workflows/prepare-environment.ts
@@ -2,6 +2,6 @@ import { logger } from "../utils/logger.ts";
 import { initTomlEditJs } from "../libs/toml-edit-js/initWasm.ts";
 
 export function prepareEnvironment() {
-  logger.info("Initializing @rainbowatcher/toml-edit-js wasm module...");
+  logger.debug("Initializing @rainbowatcher/toml-edit-js wasm module...");
   initTomlEditJs();
 }
